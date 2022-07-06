c83aefc4a89fc86846ef9e090cf8bd53e6486400
elmamborete <agustinmuller98@gmail.com>
2022-07-06 12:43:31 -0300

    ..

--- pruebas/index.js
+++ pruebas/index.js
@@ -28,8 +28,56 @@ console.log(factorial(4))
 } */
 
 
-let n = Math.random(1,10);
+/* let n = Math.random(1,10);
 console.log(n);
 function random(min, max) {
   return Math.floor((Math.random() * (max - min + 1)) + min);
 };
+ */
+
+/* function Persona(nombre, edad, calle) {
+  this.nombre = nombre;
+  this.edad   = edad;
+  this.calle  = calle;
+  this.hablar = function(){ console.log("HOLA SOY "+ this.nombre)}
+}
+const persona1 = new Persona("Homero", 39, "Av. Siempreviva 742");
+const persona2 = new Persona("Marge", 36, "Av. Siempreviva 742");
+persona1.hablar();
+console.log(persona2.edad) */
+/* const perros   = ["Pupy", "Ronnie"]
+const gatos = ["Mishi", "Garfield", "Zuri"]
+const mascotas = perros.concat(gatos)
+console.log(mascotas) */
+class Producto {
+  constructor(nombre, precio) {
+      this.id = productos.length+1
+      this.nombre  = nombre.toUpperCase();
+      this.precio  = parseFloat(precio);
+      this.vendido = false;
+  }
+  sumaIva() {
+      this.precio = this.precio * 1.21;
+  }
+}
+//Declaramos un array de productos para almacenar objetos
+const productos = [];
+productos.push(new Producto("arroz", "125"));
+productos.push(new Producto("fideos largos", "70"));
+productos.push(new Producto("pan", "50"));
+//Iteramos el array con for...of para modificarlos a todos
+for (const producto of productos)
+  producto.sumaIva();
+const espacio = [ ];
+for(let i = 0 ;i<productos.length;i++){console.log(`${productos[i].id} - ${productos[i].nombre} // ${productos[i].precio}`)}
+console.log(["."]*5)
+
+//------------------------------------
+/* class Producto {
+  constructor(id, nombre, precio) {
+    this.id  = parseInt(id);
+    this.descripion  = descripion.toUpperCase();
+    this.precio = parseFloat(precio);
+    this.stock = parseInt(stock)
+  }
+} */
